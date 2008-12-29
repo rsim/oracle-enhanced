9ca82fffd3f050d7571cbd9f8917e4647ed5d1f8
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2008-12-29 15:07:07 +0200

    added RAILS_GEM_VERSION parameter for rake spec task

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,20 +3,23 @@ gem 'rspec'
 require 'spec'
 
 $:.unshift(File.dirname(__FILE__) + '/../lib')
-# gem 'activerecord', '=2.0.2'
-# gem 'actionpack', '=2.0.2'
-# gem 'activesupport', '=2.0.2'
-# gem 'composite_primary_keys', '=0.9.93'
 
-# gem 'activerecord', '=2.1.2'
-# gem 'actionpack', '=2.1.2'
-# gem 'activesupport', '=2.1.2'
-# gem 'composite_primary_keys', '=1.0.8'
-
-gem 'activerecord', '=2.2.2'
-gem 'actionpack', '=2.2.2'
-gem 'activesupport', '=2.2.2'
-gem 'composite_primary_keys', '=2.2.0'
+if ENV['RAILS_GEM_VERSION'] =~ /^2.0/
+  gem 'activerecord', '=2.0.2'
+  gem 'actionpack', '=2.0.2'
+  gem 'activesupport', '=2.0.2'
+  gem 'composite_primary_keys', '=0.9.93'
+elsif ENV['RAILS_GEM_VERSION'] =~ /^2.1/
+  gem 'activerecord', '=2.1.2'
+  gem 'actionpack', '=2.1.2'
+  gem 'activesupport', '=2.1.2'
+  gem 'composite_primary_keys', '=1.0.8'
+else
+  gem 'activerecord', '=2.2.2'
+  gem 'actionpack', '=2.2.2'
+  gem 'activesupport', '=2.2.2'
+  gem 'composite_primary_keys', '=2.2.0'
+end
 
 require 'activerecord'
 require 'actionpack'
