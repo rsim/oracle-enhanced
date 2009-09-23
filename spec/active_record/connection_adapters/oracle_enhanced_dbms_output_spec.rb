c5092104e09b5efa656e2c86e5ab6b8495a0e7cd
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2009-09-23 10:38:24 +0300

    extracted schema dump and dbms output specs in separate files

---
+++ spec/active_record/connection_adapters/oracle_enhanced_dbms_output_spec.rb
@@ -0,0 +1,63 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe "OracleEnhancedAdapter logging dbms_output from plsql" do
+  include LoggerSpecHelper
+
+  before(:all) do
+    @buffer = StringIO.new
+    ActiveRecord::Base.establish_connection(CONNECTION_PARAMS)
+    ActiveRecord::Base.connection.execute <<-SQL
+    CREATE or REPLACE
+    FUNCTION MORE_THAN_FIVE_CHARACTERS_LONG (some_text VARCHAR2) RETURN INTEGER
+    AS
+      longer_than_five INTEGER;
+    BEGIN
+      dbms_output.put_line('before the if -' || some_text || '-');
+      IF length(some_text) > 5 THEN
+        dbms_output.put_line('it is longer than 5');
+        longer_than_five := 1;
+      ELSE
+        dbms_output.put_line('it is 5 or shorter');
+        longer_than_five := 0;
+      END IF;
+      dbms_output.put_line('about to return: ' || longer_than_five);
+      RETURN longer_than_five;
+    END;
+    SQL
+  end
+
+  before(:each) do
+    @buffer = StringIO.new
+    log_to @buffer
+    ActiveRecord::Base.establish_connection(CONNECTION_PARAMS)
+    @conn = ActiveRecord::Base.connection
+  end
+
+  it "should NOT log dbms output when dbms output is disabled" do
+    @conn.disable_dbms_output
+
+    @conn.select_all("select more_than_five_characters_long('hi there') is_it_long from dual").should == [{'is_it_long'=>1}]
+    
+    @buffer.string.should_not match(/^DBMS_OUTPUT/)
+  end
+
+  it "should log dbms output lines to the rails log" do
+    @conn.enable_dbms_output
+
+    @conn.select_all("select more_than_five_characters_long('hi there') is_it_long from dual").should == [{'is_it_long'=>1}]
+    
+    @buffer.string.should match(/^DBMS_OUTPUT: before the if -hi there-$/)
+    @buffer.string.should match(/^DBMS_OUTPUT: it is longer than 5$/)
+    @buffer.string.should match(/^DBMS_OUTPUT: about to return: 1$/)
+  end
+
+  it "should log dbms output lines to the rails log" do
+    @conn.enable_dbms_output
+
+    @conn.select_all("select more_than_five_characters_long('short') is_it_long from dual").should == [{'is_it_long'=>0}]
+    
+    @buffer.string.should match(/^DBMS_OUTPUT: before the if -short-$/)
+    @buffer.string.should match(/^DBMS_OUTPUT: it is 5 or shorter$/)
+    @buffer.string.should match(/^DBMS_OUTPUT: about to return: 0$/)
+  end
+end
--- spec/active_record/connection_adapters/oracle_enhanced_schema_spec.rb
+++ spec/active_record/connection_adapters/oracle_enhanced_schema_spec.rb
@@ -1,96 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe "OracleEnhancedAdapter schema dump" do
-
-  before(:all) do
-    if !defined?(RUBY_ENGINE)
-      if ActiveRecord::Base.respond_to?(:oracle_connection)
-        @old_conn = ActiveRecord::Base.oracle_connection(CONNECTION_PARAMS)
-        @old_conn.class.should == ActiveRecord::ConnectionAdapters::OracleAdapter
-      end
-    elsif RUBY_ENGINE == 'jruby'
-      @old_conn = ActiveRecord::Base.jdbc_connection(JDBC_CONNECTION_PARAMS)
-      @old_conn.class.should == ActiveRecord::ConnectionAdapters::JdbcAdapter
-    end
-
-    @new_conn = ActiveRecord::Base.oracle_enhanced_connection(CONNECTION_PARAMS)
-    @new_conn.class.should == ActiveRecord::ConnectionAdapters::OracleEnhancedAdapter
-  end
-
-  after(:all) do
-    # Workaround for undefining callback that was defined by JDBC adapter
-    if defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
-      ActiveRecord::Base.class_eval do
-        def after_save_with_oracle_lob
-          nil
-        end
-      end
-    end
-  end
-
-  if @old_conn
-    it "should return the same tables list as original oracle adapter" do
-      @new_conn.tables.sort.should == @old_conn.tables.sort
-    end
-
-    it "should return the same index list as original oracle adapter" do
-      @new_conn.indexes('employees').sort_by(&:name).should == @old_conn.indexes('employees').sort_by(&:name)
-    end
-
-    it "should return the same pk_and_sequence_for as original oracle adapter" do
-      if @old_conn.respond_to?(:pk_and_sequence_for)
-        @new_conn.tables.each do |t|
-          @new_conn.pk_and_sequence_for(t).should == @old_conn.pk_and_sequence_for(t)
-        end
-      end
-    end
-
-    it "should return the same structure dump as original oracle adapter" do
-      @new_conn.structure_dump.split(";\n\n").sort.should == @old_conn.structure_dump.split(";\n\n").sort
-    end
-
-    it "should return the same structure drop as original oracle adapter" do
-      @new_conn.structure_drop.split(";\n\n").sort.should == @old_conn.structure_drop.split(";\n\n").sort
-    end
-  end
-
-  it "should return the character size of nvarchar fields" do
-    @new_conn.execute <<-SQL
-      CREATE TABLE nvarchartable (
-        session_id  NVARCHAR2(255) DEFAULT NULL
-      )
-    SQL
-    if /.*session_id nvarchar2\((\d+)\).*/ =~ @new_conn.structure_dump
-       "#$1".should == "255"
-    end
-    @new_conn.execute "DROP TABLE nvarchartable"
-  end
-end
-
-describe "OracleEnhancedAdapter" do
+describe "OracleEnhancedAdapter schema definition" do
   before(:all) do
     ActiveRecord::Base.establish_connection(CONNECTION_PARAMS)
     @conn = ActiveRecord::Base.connection
-  end
-
-  describe "database stucture dump extentions" do
-    before(:all) do
-      @conn.execute <<-SQL
-        CREATE TABLE nvarchartable (
-          unq_nvarchar  NVARCHAR2(255) DEFAULT NULL
-        )
-      SQL
-    end
-
-    after(:all) do
-      @conn.execute "DROP TABLE nvarchartable"
-    end
-
-    it "should return the character size of nvarchar fields" do
-      if /.*unq_nvarchar nvarchar2\((\d+)\).*/ =~ @conn.structure_dump
-         "#$1".should == "255"
-      end
-    end
   end
 
   describe "table and sequence creation with non-default primary key" do
