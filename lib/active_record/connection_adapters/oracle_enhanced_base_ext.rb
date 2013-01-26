f50aa48b27eda4f0e0b57e769861ed4f5ed0899c
Benjamin Ortega <ben.ortega@gmail.com>
2013-01-25 22:55:22 -0600

    always write serialized LOB columns #275

--- lib/active_record/connection_adapters/oracle_enhanced_base_ext.rb
+++ lib/active_record/connection_adapters/oracle_enhanced_base_ext.rb
@@ -75,7 +75,10 @@ module ActiveRecord
       end
     end
     def record_changed_lobs
-      @changed_lob_columns = self.class.lob_columns.select{|col| self.send(:"#{col.name}_changed?") && !self.class.readonly_attributes.to_a.include?(col.name) || self.class.serialized_attributes.keys && !self.class.readonly_attributes.to_a.include?(col.name)}
+      @changed_lob_columns = self.class.lob_columns.select do |col|
+        self.class.serialized_attributes.keys.include?(col.name) ||
+          (self.send(:"#{col.name}_changed?") && !self.class.readonly_attributes.to_a.include?(col.name))
+      end
     end
     private :enhanced_write_lobs
     private :record_changed_lobs
