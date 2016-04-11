ce978943559807f40fd29459f214386fd2a91703
Yasuo Honda <yasuo.honda@gmail.com>
2016-04-11 20:32:26 +0000

    Support bind_params for JDBC connections

--- lib/active_record/connection_adapters/oracle_enhanced/jdbc_connection.rb
+++ lib/active_record/connection_adapters/oracle_enhanced/jdbc_connection.rb
@@ -329,6 +329,18 @@ module ActiveRecord
           @raw_statement = raw_statement
         end
 
+        def bind_params( *bind_vars )
+          index = 1
+          bind_vars.flatten.each do |var|
+            if Hash === var
+              var.each { |key, val| bind_param key, val }
+            else
+              bind_param index, var
+              index += 1
+            end
+          end
+        end
+
         def bind_param(position, value, column = nil)
           col_type = column && column.type
           java_value = ruby_to_java_value(value, col_type)
