fa3fab1a6405167ecff46cf7f9bafac7ff4af32f
Yasuo Honda <yasuo.honda@gmail.com>
2016-04-12 12:29:25 +0000

    Restore necessary quote for Date

--- lib/active_record/connection_adapters/oracle_enhanced/quoting.rb
+++ lib/active_record/connection_adapters/oracle_enhanced/quoting.rb
@@ -89,8 +89,11 @@ module ActiveRecord
         end
 
         def _quote(value) #:nodoc:
-          if value.is_a? ActiveModel::Type::Binary::Data
+          case value
+          when ActiveModel::Type::Binary::Data then
             %Q{empty_#{ type_to_sql(column.type.to_sym).downcase rescue 'blob' }()}
+          when Date then
+            quote_date_with_to_date(value)
           else
             super
           end
