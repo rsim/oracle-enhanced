66f30c755d412e7949d44fed3b95997342200656
Yasuo Honda <yasuo.honda@gmail.com>
2017-02-22 14:57:03 +0000

    Suppress `add_index_options` method `DEPRECATION WARNING: Passing default to #index_name_exists? is deprecated without replacement.`

--- lib/active_record/connection_adapters/oracle_enhanced/schema_statements.rb
+++ lib/active_record/connection_adapters/oracle_enhanced/schema_statements.rb
@@ -161,7 +161,7 @@ module ActiveRecord
           if index_name.to_s.length > max_index_length
             raise ArgumentError, "Index name '#{index_name}' on table '#{table_name}' is too long; the limit is #{max_index_length} characters"
           end
-          if index_name_exists?(table_name, index_name, false)
+          if table_exists?(table_name) && index_name_exists?(table_name, index_name)
             raise ArgumentError, "Index name '#{index_name}' on table '#{table_name}' already exists"
           end
 
