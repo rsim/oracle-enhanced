d7e5d51bbbc67cdcf2224e99ea88ef75a32edea6
Yasuo Honda <yasuo.honda@gmail.com>
2017-03-30 16:59:13 +0000

    Let abstract adapter type cast Date, DateTime and Time for JRuby Refer #1267 for CRuby

--- lib/active_record/connection_adapters/oracle_enhanced/jdbc_quoting.rb
+++ lib/active_record/connection_adapters/oracle_enhanced/jdbc_quoting.rb
@@ -12,10 +12,6 @@ module ActiveRecord
             clob = Java::OracleSql::CLOB.createTemporary(@connection.raw_connection, false, Java::OracleSql::CLOB::DURATION_SESSION)
             clob.setString(1, value.to_s)
             clob
-          when Date, DateTime
-            Java::oracle.sql.DATE.new(value.strftime("%Y-%m-%d %H:%M:%S"))
-          when Time
-            Java::java.sql.Timestamp.new(value.year - 1900, value.month - 1, value.day, value.hour, value.min, value.sec, value.usec * 1000)
           else
             super
           end
