db4464129f2d68bd0f17a362f95c1673a3024fa0
Yasuo Honda <yasuo.honda@gmail.com>
2017-06-01 14:03:31 +0000

    requires ruby-oci8 2.2.4 or higher

--- lib/active_record/connection_adapters/oracle_enhanced/oci_connection.rb
+++ lib/active_record/connection_adapters/oracle_enhanced/oci_connection.rb
@@ -10,7 +10,7 @@ rescue LoadError => e
 end
 
 # check ruby-oci8 version
-required_oci8_version = [2, 2, 0]
+required_oci8_version = [2, 2, 4]
 oci8_version_ints = OCI8::VERSION.scan(/\d+/).map { |s| s.to_i }
 if (oci8_version_ints <=> required_oci8_version) < 0
   raise LoadError, "ERROR: ruby-oci8 version #{OCI8::VERSION} is too old. Please install ruby-oci8 version #{required_oci8_version.join('.')} or later."
