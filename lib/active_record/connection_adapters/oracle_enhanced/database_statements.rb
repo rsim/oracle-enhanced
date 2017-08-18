b2159b37ddee38c09c4497c2a16068c83e6560d9
Koichi ITO <koic.ito@gmail.com>
2017-08-18 15:06:51 +0900

    Restore `to_sql` to return only SQL
    Follow up of rails/rails#29945.

--- lib/active_record/connection_adapters/oracle_enhanced/database_statements.rb
+++ lib/active_record/connection_adapters/oracle_enhanced/database_statements.rb
@@ -64,8 +64,7 @@ module ActiveRecord
         end
 
         def explain(arel, binds = [])
-          sql, binds = to_sql(arel, binds)
-          sql = "EXPLAIN PLAN FOR #{sql}"
+          sql = "EXPLAIN PLAN FOR #{to_sql(arel, binds)}"
           return if sql =~ /FROM all_/
           if ORACLE_ENHANCED_CONNECTION == :jdbc
             exec_query(sql, "EXPLAIN", binds)
