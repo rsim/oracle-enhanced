7648d0e60c1848fc89672cc7d84bf2f4a721b679
Guilherme Portugues <g_portugues@hotmail.com>
2018-09-06 14:07:35 -0300

    table with quoted name (case sensitive) and synonym

--- lib/active_record/connection_adapters/oracle_enhanced/connection.rb
+++ lib/active_record/connection_adapters/oracle_enhanced/connection.rb
@@ -24,8 +24,11 @@ module ActiveRecord
           def describe(name)
             name = name.to_s
             if name.include?("@")
-              raise ArgumentError "db link is not supported"
+              name, db_link = name.split("@")
+              default_owner = _select_value("SELECT username FROM all_db_links WHERE db_link = '#{db_link.upcase}'")
+              db_link = "@#{db_link}"
             else
+              db_link = nil
               default_owner = @owner
             end
             real_name = OracleEnhanced::Quoting.valid_table_name?(name) ? name.upcase : name
@@ -34,33 +37,38 @@ module ActiveRecord
             else
               table_owner, table_name = default_owner, real_name
             end
+            table_name.sub! '"', ''
             sql = <<-SQL.strip.gsub(/\s+/, " ")
           SELECT owner, table_name, 'TABLE' name_type
-          FROM all_tables
+          FROM all_tables#{db_link}
           WHERE owner = '#{table_owner}'
             AND table_name = '#{table_name}'
           UNION ALL
           SELECT owner, view_name table_name, 'VIEW' name_type
-          FROM all_views
+          FROM all_views#{db_link}
           WHERE owner = '#{table_owner}'
             AND view_name = '#{table_name}'
           UNION ALL
-          SELECT table_owner, table_name, 'SYNONYM' name_type
-          FROM all_synonyms
+          SELECT table_owner, DECODE(db_link, NULL, table_name, table_name||'@'||db_link), 'SYNONYM' name_type
+          FROM all_synonyms#{db_link}
           WHERE owner = '#{table_owner}'
             AND synonym_name = '#{table_name}'
           UNION ALL
-          SELECT table_owner, table_name, 'SYNONYM' name_type
-          FROM all_synonyms
+          SELECT table_owner, DECODE(db_link, NULL, table_name, table_name||'@'||db_link), 'SYNONYM' name_type
+          FROM all_synonyms#{db_link}
           WHERE owner = 'PUBLIC'
             AND synonym_name = '#{real_name}'
         SQL
             if result = _select_one(sql)
               case result["name_type"]
               when "SYNONYM"
-                describe("#{result['owner'] && "#{result['owner']}."}#{result['table_name']}")
+                if result['table_name'] == result['table_name'].upcase
+                  describe("#{result['owner'] && "#{result['owner']}."}#{result['table_name']}#{db_link}")
+                else
+                  describe("#{result['owner'] && "#{result['owner']}."}\"#{result['table_name']}\"#{db_link}")
+                end
               else
-                [result["owner"], result["table_name"]]
+                db_link ? [result["owner"], result["table_name"], db_link] : [result["owner"], result["table_name"]]
               end
             else
               raise OracleEnhanced::ConnectionException, %Q{"DESC #{name}" failed; does it exist?}
