cf6265e5f8ad69d00060633e64d75ff239d9526b
Yasuo Honda <yasuo.honda@gmail.com>
2020-07-07 21:36:22 +0900

    Use Oracle Instant Client 18.5
    To meet the same timezone file version between Oracle client and server.
    Fixes #2033

--- ci/setup_accounts.sh
+++ ci/setup_accounts.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-/opt/oracle/product/18c/dbhomeXE/bin/sqlplus system/Oracle18@XEPDB1 <<SQL
+/usr/lib/oracle/18.5/client64/bin/sqlplus system/Oracle18@XEPDB1 <<SQL
 @@spec/support/alter_system_user_password.sql
 @@spec/support/alter_system_set_open_cursors.sql
 @@spec/support/create_oracle_enhanced_users.sql
