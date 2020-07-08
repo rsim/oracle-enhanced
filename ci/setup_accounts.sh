14e5ea5118ee2cd456b45bcd9a5debb9870c599a
Yasuo Honda <yasuo.honda@gmail.com>
2020-07-08 23:02:19 +0900

    Use environment variables for sqlplus

--- ci/setup_accounts.sh
+++ ci/setup_accounts.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-/usr/lib/oracle/18.5/client64/bin/sqlplus system/Oracle18@XEPDB1 <<SQL
+${ORACLE_HOME}/bin/sqlplus system/${DATABASE_SYS_PASSWORD}@${DATABASE_NAME} <<SQL
 @@spec/support/alter_system_user_password.sql
 @@spec/support/alter_system_set_open_cursors.sql
 @@spec/support/create_oracle_enhanced_users.sql
