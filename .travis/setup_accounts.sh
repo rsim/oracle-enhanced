f34e9e7d7e5e924a543775fed5a7dab8b9404e12
Yasuo Honda <yasuo.honda@gmail.com>
2021-01-12 20:51:02 +0900

    Revert "Disable Travis CI"

---
+++ .travis/setup_accounts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ev
+
+"$ORACLE_HOME/bin/sqlplus" -L -S / AS SYSDBA <<SQL
+@@spec/support/alter_system_user_password.sql
+@@spec/support/alter_system_set_open_cursors.sql
+@@spec/support/create_oracle_enhanced_users.sql
+exit
+SQL
