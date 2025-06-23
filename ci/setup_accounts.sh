bc9533d3ec8b0487cab5a669995343f5e719be56
Yasuo Honda <yasuo.honda@gmail.com>
2025-06-23 16:06:46 +0900

    Address ORA-01031: insufficient privileges
    This commit addresses the following ORA-01031 error.
    
    ```
    GRANT EXECUTE ON dbms_lock TO ruby
                     *
    ERROR at line 1:
    ORA-01031: insufficient privileges
    Help: https://docs.oracle.com/error-help/db/ora-01031/
    ```

--- ci/setup_accounts.sh
+++ ci/setup_accounts.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-sqlplus system/${DATABASE_SYS_PASSWORD}@${DATABASE_NAME} <<SQL
+sqlplus sys/${DATABASE_SYS_PASSWORD}@${DATABASE_NAME} as sysdba<<SQL
 @@spec/support/alter_system_set_open_cursors.sql
 @@spec/support/create_oracle_enhanced_users.sql
 exit
