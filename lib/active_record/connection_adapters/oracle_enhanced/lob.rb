93aeb389f49f187aee7d8ff1af8bd177ed6fcbd0
Yasuo Honda <yasuo.honda@gmail.com>
2026-02-03 23:13:18 +0900

    Merge pull request #2485 from andynu/lob-clob-fix
    Fix CLOB/BLOB insertion when prepared_statements is false

--- lib/active_record/connection_adapters/oracle_enhanced/lob.rb
+++ lib/active_record/connection_adapters/oracle_enhanced/lob.rb
@@ -11,6 +11,8 @@ module ActiveRecord # :nodoc:
 
           # After setting large objects to empty, select the OCI8::LOB
           # and write back the data.
+          before_create :record_lobs_for_create
+          after_create :enhanced_write_lobs
           before_update :record_changed_lobs
           after_update :enhanced_write_lobs
         end
@@ -25,11 +27,21 @@ module ActiveRecord # :nodoc:
 
         private
           def enhanced_write_lobs
+            # Skip if using prepared statements - LOB data is already written via temp LOB binding
+            return if self.class.connection.prepared_statements
+
             if self.class.connection.is_a?(ConnectionAdapters::OracleEnhancedAdapter) &&
                 !(self.class.custom_create_method || self.class.custom_update_method)
               self.class.connection.write_lobs(self.class.table_name, self.class, attributes, @changed_lob_columns)
             end
           end
+
+          def record_lobs_for_create
+            @changed_lob_columns = self.class.lob_columns.select do |col|
+              !attributes[col.name].nil? && !self.class.readonly_attributes.to_a.include?(col.name)
+            end
+          end
+
           def record_changed_lobs
             @changed_lob_columns = self.class.lob_columns.select do |col|
               self.will_save_change_to_attribute?(col.name) && !self.class.readonly_attributes.to_a.include?(col.name)
