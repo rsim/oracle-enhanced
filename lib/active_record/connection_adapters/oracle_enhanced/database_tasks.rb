1e437b046688834fd763e474cba984ccd124ebd4
Yasuo Honda <yasuo.honda@gmail.com>
2026-04-20 22:37:56 +0900

    Backport #2556: Implement check_current_protected_environment! for Rails 8.1+
    Cherry-picks merge commit 81f5226981d82b8e942f9bd368c37e2fb50d74ca onto
    release81. Refactors OracleEnhancedAdapter::DatabaseTasks to inherit from
    ActiveRecord::Tasks::AbstractTasks so Rails 8.1's
    ActiveRecord::Tasks::DatabaseTasks#check_protected_environments! dispatches
    cleanly through the adapter. Adds a regression spec that drives the dispatch
    end-to-end.
    
    Cherry-pick applied cleanly (no conflicts) on top of #2563 (#2559 backport).
    
    Fixes #2540.
    
    (cherry picked from commit 81f5226981d82b8e942f9bd368c37e2fb50d74ca)

--- lib/active_record/connection_adapters/oracle_enhanced/database_tasks.rb
+++ lib/active_record/connection_adapters/oracle_enhanced/database_tasks.rb
@@ -1,40 +1,35 @@
 # frozen_string_literal: true
 
 require "active_record/base"
+require "active_record/tasks/abstract_tasks"
 
 module ActiveRecord
   module ConnectionAdapters
     class OracleEnhancedAdapter
-      class DatabaseTasks
-        delegate :connection, :establish_connection, to: ActiveRecord::Base
-
-        def initialize(config)
-          @config = config
-        end
-
+      class DatabaseTasks < ActiveRecord::Tasks::AbstractTasks
         def create
           system_password = ENV.fetch("ORACLE_SYSTEM_PASSWORD") {
             print "Please provide the SYSTEM password for your Oracle installation (set ORACLE_SYSTEM_PASSWORD to avoid this prompt)\n>"
             $stdin.gets.strip
           }
-          establish_connection(@config.merge(username: "SYSTEM", password: system_password))
+          establish_connection(configuration_hash.merge(username: "SYSTEM", password: system_password))
           begin
-            connection.execute "CREATE USER #{@config[:username]} IDENTIFIED BY #{@config[:password]}"
+            connection.execute "CREATE USER #{configuration_hash[:username]} IDENTIFIED BY #{configuration_hash[:password]}"
           rescue => e
             if /ORA-01920/.match?(e.message) # user name conflicts with another user or role name
-              connection.execute "ALTER USER #{@config[:username]} IDENTIFIED BY #{@config[:password]}"
+              connection.execute "ALTER USER #{configuration_hash[:username]} IDENTIFIED BY #{configuration_hash[:password]}"
             else
               raise e
             end
           end
 
           OracleEnhancedAdapter.permissions.each do |permission|
-            connection.execute "GRANT #{permission} TO #{@config[:username]}"
+            connection.execute "GRANT #{permission} TO #{configuration_hash[:username]}"
           end
         end
 
         def drop
-          establish_connection(@config)
+          establish_connection
           connection.execute_structure_dump(connection.full_drop)
         end
 
@@ -44,15 +39,15 @@ module ActiveRecord
         end
 
         def structure_dump(filename, extra_flags)
-          establish_connection(@config)
+          establish_connection
           File.open(filename, "w:utf-8") { |f| f << connection.structure_dump }
-          if @config[:structure_dump] == "db_stored_code"
+          if configuration_hash[:structure_dump] == "db_stored_code"
             File.open(filename, "a:utf-8") { |f| f << connection.structure_dump_db_stored_code }
           end
         end
 
         def structure_load(filename, extra_flags)
-          establish_connection(@config)
+          establish_connection
           connection.execute_structure_dump(File.read(filename))
         end
       end
